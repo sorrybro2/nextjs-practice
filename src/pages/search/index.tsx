f256a31111ab4ec87aa93a78b5ad95435b2c384f
IM_SORRY <imwe25@naver.com>
2025-12-11 22:09:46 +0900

    feat: useRouter 사용해봄

--- src/pages/search/index.tsx
+++ src/pages/search/index.tsx
@@ -1,3 +1,10 @@
+// router라는 객체를 컴포넌트 내부에서 사용할 수 있도록 반환하는 함수
+import { useRouter } from "next/router"
+
 export default function Page() {
-    return <h1>Search</h1>
+    const router = useRouter();
+
+    const { q } = router.query;
+
+    return <h1>Search {q}</h1>
 }
