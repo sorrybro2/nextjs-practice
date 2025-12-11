44d44002454ade55b9ecb42beea986331da592c3
IM_SORRY <imwe25@naver.com>
2025-12-11 21:08:14 +0900

    first commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,11 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
+// Component : 현재 페이지 역할을 할 컴포넌트
+// pageProps : 페이지에 전달된 props들 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return <>
+    <header>글로벌 헤더</header>
+    <Component {...pageProps} />
+  </>;
 }
